1304f3d85b8885d9150de17f40ae8683b01beebd
Izth4rDev <miguel.rondanelli.inf@gmail.com>
2024-04-11 22:15:02 -0400

    borrado de import duplicado

--- src/cl/praxis/calculoResistencia/Main.java
+++ src/cl/praxis/calculoResistencia/Main.java
@@ -2,7 +2,7 @@ package cl.praxis.calculoResistencia;
 import java.util.Scanner;
 import java.awt.desktop.SystemEventListener;
 import java.sql.SQLOutput;
-import java.util.Scanner;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
